eb5ca1fee710882e9ada2d54c388c2e44fd0eca6
Yaroslav Ilin <nilfalse@yandex.com>
2020-08-08 10:40:30 +0200

    save links for testing articles

--- src/background.ts
+++ src/background.ts
@@ -1,1 +1,7 @@
 console.log('hello flags');
+
+// https://www.streaver.com/blog/posts/testing-web-extensions.html
+// https://github.com/alisterscott/playwright-demo/blob/master/specs/demo-spec1.js
+// https://github.com/microsoft/playwright/issues/558
+// https://github.com/mozilla/web-ext/issues/1927#issuecomment-647842800
+// https://github.com/aslushnikov/demo-playwright-with-firefox-web-extension
