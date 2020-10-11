ed3a6fc9e2cf269432be9c8129199ae8bba6ee03
Yaroslav Ilin <nilfalse@yandex.com>
2020-10-11 22:08:49 +0200

    Add Popup-related fixtures & harness

---
+++ src/popup/__fixtures__/success_ip-ch.ts
@@ -0,0 +1,17 @@
+export const state = 'success' as const;
+
+export const request = {
+  url: 'https://protonmail.com/',
+  ip: '185.70.41.35',
+};
+
+export const matches = [
+  {
+    heuristic: 'ip',
+    score: 0.5,
+    isoCountry: 'CH',
+    isoRegion: null,
+    continent: 'EU',
+    extra: { registeredCountry: 'CH' },
+  },
+] as const;
--- src/view/components/popup_content.tsx
+++ src/view/components/popup_content.tsx
@@ -3,15 +3,13 @@ import { hot } from 'react-hot-loader/root';
 import * as React from 'react';
 
 import { usePopupContent } from '../hooks/popup_content';
-import { useQueryParams } from '../hooks/query_params';
 
 import { Empty } from './empty';
 import { HeaderPrimary } from './header_primary';
 import { Traceroute } from './traceroute';
 
 export const _PopupContent: React.FC = () => {
-  const { tab } = useQueryParams();
-  const popupResponse = usePopupContent(typeof tab === 'string' ? tab : null);
+  const popupResponse = usePopupContent();
 
   if (popupResponse.state === 'success' && popupResponse.details) {
     return (
