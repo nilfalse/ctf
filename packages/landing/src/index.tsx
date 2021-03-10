10779994824438285a506c822e1f32f991c17e89
Yaroslav Ilin <nilfalse@yandex.com>
2021-03-10 23:33:14 +0100

    Bump Github Actions cache

--- packages/landing/src/index.tsx
+++ packages/landing/src/index.tsx
@@ -6,11 +6,22 @@ import { reportWebVitals } from './report-web-vitals';
 
 import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('react-root')
-);
+const rootElement = document.getElementById('react-root');
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+if (rootElement!.hasChildNodes()) {
+  ReactDOM.hydrate(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    rootElement
+  );
+} else {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    rootElement
+  );
+}
 
 reportWebVitals();
