b4cf28c8c5f88bb2e36f7ed0bb0520a1538f9b19
Yaroslav Ilin <nilfalse@yandex.com>
2021-03-11 01:12:59 +0100

    Use strict cache hit policy on Github Actions

--- packages/landing/src/app.tsx
+++ packages/landing/src/app.tsx
@@ -5,14 +5,31 @@ export function App() {
     <div className="app">
       <header className="app-header">
         <p>Capture The Flag</p>
-        <a
-          className="app-link"
-          href="https://addons.mozilla.org/firefox/addon/ctf/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Install extension
-        </a>
+        <small>Install extension:</small>
+
+        <ul>
+          <li>
+            <a
+              className="app-link"
+              href="https://addons.mozilla.org/firefox/addon/ctf/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Firefox
+            </a>
+          </li>
+
+          <li>
+            <a
+              className="app-link"
+              href="https://chrome.google.com/webstore/detail/plmbleiamgcdnenigiocddjjgacgfgjb"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Chrome
+            </a>
+          </li>
+        </ul>
       </header>
     </div>
   );
